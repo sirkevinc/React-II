ff8963f1d6e1ecb0583e9dd9820ccf5507cfd19d
Kevin <16568166+sirkevinc@users.noreply.github.com>
2018-02-06 19:57:00 -0500

    App.js Update
    Remove default content
    
    Edit App component to contain and render SearchBar
    
    Iterate over dummyData using map and pass each object
    as a prop to each instance of PostContainer

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -7,14 +7,9 @@ import PostContainer from './components/PostContainer/PostContainer.js';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <SearchBar />
+        {dummyData.map((post, i) => <PostContainer key={i} postData={post} />)}
       </div>
     );
   }
