b7805d887af27ed1248577b84d9953f54facaccd
Kevin <16568166+sirkevinc@users.noreply.github.com>
2018-02-06 19:35:14 -0500

    Import Required Files
    Import files needed for App.js to function
    Import dummyData, SearchBar and PostContainer files

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import dummyData from './dummy-data';
+import SearchBar from './components/SearchBar/SearchBar.js';
+import PostContainer from './components/PostContainer/PostContainer.js';
 
 class App extends Component {
   render() {
